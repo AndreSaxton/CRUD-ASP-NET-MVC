a02c701d69d135b920ebed566df6828894cd5bf0
André Saxton <andre_saxton@hotmail.com>
2018-12-05 00:16:31 -0200

    Criado views, controllers e model

--- ASP NET MVC CRUD/App_Start/RouteConfig.cs
+++ ASP NET MVC CRUD/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ASP_NET_MVC_CRUD
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- ASP NET MVC CRUD/Controllers/HomeController.cs
+++ ASP NET MVC CRUD/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP_NET_MVC_CRUD.Models.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,21 +9,26 @@ namespace ASP_NET_MVC_CRUD.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Page2()
         {
-            ViewBag.Message = "Your application description page.";
+            var vm = new Page2Model();
+            vm.FistName = "Default Name";
+            //vm.LastName = ;
+            //vm.Age = ;
 
-            return View();
+            return View(vm);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Page2(string firstName, string lastName, int? age)
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Result = String.Format("{0} {1}; Age {2}", firstName, lastName, age);
 
             return View();
         }
