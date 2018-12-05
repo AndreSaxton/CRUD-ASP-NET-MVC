7ec5f1673d13e07960eb3fff4ae681c7bff87ed9
André Saxton <andre_saxton@hotmail.com>
2018-12-05 00:37:56 -0200

    adicionado mensagem de erro no model

--- ASP NET MVC CRUD/Models/Home/Page2Model.cs
+++ ASP NET MVC CRUD/Models/Home/Page2Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace ASP_NET_MVC_CRUD.Models.Home
 {
     public class Page2Model
     {
+        [Required(ErrorMessage ="You are a failure when it come to entering form data.")]
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public int? Age { get; set; }
     }
 }
